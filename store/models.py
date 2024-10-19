a766c07ca3773b5ab20cbb09c07cf16c2b9e1952
rohit-rajput-1 <rohit31rajput2001@gmail.com>
2024-10-19 18:32:23 +0530

    Feature Registered in models.py

--- store/models.py
+++ store/models.py
@@ -29,7 +29,9 @@ class Product(models.Model):
     category = models.ForeignKey(Category,on_delete=models.CASCADE,default=1)
     description = models.CharField(max_length=300,default='',null=True,blank=True)
     image = models.ImageField(upload_to='uploads/product/')
-
+    # Add Sales Details
+    is_sale = models.BooleanField(default=False)
+    sale_price = models.DecimalField(default=0,max_digits=8,decimal_places=2)
     def __str__(self):
         return self.name
     
