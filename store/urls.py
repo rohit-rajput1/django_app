c0c123d2052c0f407d30f30c0c127b7673aa78a7
rohit-rajput-1 <rohit31rajput2001@gmail.com>
2025-02-15 16:49:39 +0530

    register page code added

--- store/urls.py
+++ store/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('',views.home,name='home'),
     path('about/',views.about,name='about'),
     path('login/',views.login_user,name='login'),
+    # We have created logout url but not a page because we want a redirect it to home.
     path('logout/',views.logout_user,name='logout'),
+    path('register/',views.register_user,name='register'),
 ]
--- store/views.py
+++ store/views.py
@@ -2,10 +2,36 @@ from django.shortcuts import render,redirect
 from .models import Product
 from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
+from .forms import SignUpForm
 
 def home(request):
 	products = Product.objects.all()
 	return render(request, 'index.html', {'products':products})
+
+def register_user(request):
+    form = SignUpForm()
+    
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+
+            # Authenticate & Login the User
+            user = authenticate(request, username=username, password=password)
+            if user:
+                login(request, user)
+                messages.success(request, "You have registered successfully.")
+                return redirect('home')
+            else:
+                messages.error(request, "Authentication failed. Please log in manually.")
+                return redirect('login')
+        else:
+            messages.error(request, "Error occurred! Please check the form.")
+    
+    return render(request, 'register.html', {'form': form})
 
 def about(request):
 	return render(request, 'about.html',{})
@@ -24,7 +50,6 @@ def login_user(request):
             return redirect('login')
     else:
         return render(request, 'login.html', {})
-
 
 
 def logout_user(request):
